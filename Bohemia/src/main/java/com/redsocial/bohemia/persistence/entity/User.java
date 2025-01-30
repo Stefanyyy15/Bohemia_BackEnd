31f95a4035b5634956cc9c0a8108ac88b3b758ce
miDaya02 <dannyjair112@gmail.com>
2025-01-30 13:00:20 -0500

    feat: :art: We changed the User class

--- Bohemia/src/main/java/com/redsocial/bohemia/persistence/entity/User.java
+++ Bohemia/src/main/java/com/redsocial/bohemia/persistence/entity/User.java
@@ -1,10 +1,22 @@
 
 package com.redsocial.bohemia.persistence.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "users")
